a91152b52600d22bd9b5f17a73ad832bb4070f8c
Danijel Huis <danijel.huis.app@gmail.com>
2025-03-04 15:20:15 +0100

    Fastlane SDK release 0.0.1.

--- Package.swift
+++ Package.swift
@@ -1,4 +1,5 @@
-// swift-tools-version: 6.0
+// swift-tools-version: 5.10
+// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
@@ -10,32 +11,31 @@ let package = Package(
     products: [
         .library(
             name: "IACore",
-            targets: ["IACore"]
-        ),
+            targets: ["IACore"]),
         .library(
             name: "IAOverTheCounter",
-            targets: ["IAOverTheCounterWrapperTarget"]
-        )
+            targets: ["IAOverTheCounterWrapper"])
+    ],
+    dependencies: [
     ],
     targets: [
         .binaryTarget(
             name: "IACore",
-            url: "https://github.com/ihreapotheken/IA-SDK-iOS/releases/download/0.0.1-beta1/IACore-0.0.1.xcframework.zip",
-            checksum: "d0c351faf69eb9f21810d3f492b478ee2a0322c6871d6a0bfae07ee870841048"
+            url: "https://github.com/danijel-huis-4ofthem/TestRepo/releases/download/0.0.1/IACore-0.0.1.xcframework.zip",
+            checksum: "6d79a0289928cb499d1d1271a1b24c5c4da69c55ab5526f09251adfe7c805fb4"
         ),
         .binaryTarget(
             name: "IAOverTheCounter",
-            url: "https://github.com/ihreapotheken/IA-SDK-iOS/releases/download/0.0.1-beta1/IAOverTheCounter-0.0.1.xcframework.zip",
-            checksum: "429df1ad0f2e2b0a4e4673a59ba2761c6cde3376acad48f8bf7ebd2f7d21f905"
+            url: "https://github.com/danijel-huis-4ofthem/TestRepo/releases/download/0.0.1/IAOverTheCounter-0.0.1.xcframework.zip",
+            checksum: "4bd8c7f66b7c0b4ec8eaea5e92841dc18aaabf6ee186bcd20496630597b5d24a"
         ),
         .target(
-            name: "IAOverTheCounterWrapperTarget",
+            name: "IAOverTheCounterWrapper",
             dependencies: [ 
                 .target(name: "IACore"),
                 .target(name: "IAOverTheCounter"),
             ],
-            path: "IAOverTheCounterWrapperTarget"
+            path: "Sources"
         )
     ]
 )
-
